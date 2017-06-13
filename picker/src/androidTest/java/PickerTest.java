50480d8179704469b1ab7c9e89e12a9023b9eefd
Administrator <nicolas.dierick@peoplbrain.com>
2017-06-13 15:01:58 +0200

    clean

---
+++ picker/src/androidTest/java/PickerTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Created by NicolasD on 13/06/2017.
+ * Copyright Allianzes
+ */
+
+public class PickerTest {
+
+    @Test
+    public void helloWorld(){
+        assertTrue("Is true === TRUE ? ", true);
+    }
+
+    @Test
+    public void helloWorld2(){
+        assertTrue("Is true 1 === TRUE ? ", true);
+    }
+
+    @Test
+    public void helloWorld3(){
+        assertTrue("Is true 2 === TRUE ? ", false);
+    }
+
+}
